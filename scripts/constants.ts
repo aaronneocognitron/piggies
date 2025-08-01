f241143f6d1a2d3b489606fe4898af49b3b27382
aaronneocognitron <aaron.neocognitron@gmail.com>
2025-08-01 21:17:13 +0300

    move contract addresses to .env

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -1,31 +1,7 @@
 import { Address } from "@ton/core";
 
-// export const ContractAddresses = {
-//   pigShop: Address.parse("EQCveU71E-axXTvgOnr6QrXTEtgG0cXgk6Vu0ZmP0Iq-ilq_"),
-
-//   pigShopRawString: "0:b893a0cdb567a0de901990f956c479332734468bda72c5b8638f5551dffea3ab",
-
-//   pigsCollection: Address.parse("EQACbw6H4xpZlysyQUYwwfW4fOtUgLdpEZP1aTzdj6_-D_AB"),
-
-//   PigShopCollectionRawString:
-//     "0:026f0e87e31a59972b32414630c1f5b87ceb5480b7691193f5693cdd8faffe0f",
-
-// };
-
 export const ContractAddresses = {
-  pigShop: Address.parse("EQBg0eFOp490X8Ngqob8hcyuOgHFdpaAxYgh3NbEzumsJLiu"),
+  pigShop: Address.parse(process.env.NEXT_PUBLIC_PIG_SHOP_ADDRESS!),
 
-  pigsCollection: Address.parse("EQAqWnqoT4edXmwKvH_6ATnkZShrvilO5HtZmgkHkcYp8NC_"),
+  pigsCollection: Address.parse(process.env.NEXT_PUBLIC_PIGS_COLLECTION_ADDRESS!),
 };
-
-// Connected to wallet at address: EQBfJH9a8I9C8eU0iu9wRkcWBmNtUZmJzeHGi1IcCTaoHfZr
-// Sent transaction
-// Contract deployed at address EQBg0eFOp490X8Ngqob8hcyuOgHFdpaAxYgh3NbEzumsJLiu
-// You can view it at https://tonscan.org/address/EQBg0eFOp490X8Ngqob8hcyuOgHFdpaAxYgh3NbEzumsJLiu
-// PigShop contract deployed successfully at: 0:60d1e14ea78f745fc360aa86fc85ccae3a01c5769680c58821dcd6c4cee9ac24
-// Sent transaction
-// Contract deployed at address EQAqWnqoT4edXmwKvH_6ATnkZShrvilO5HtZmgkHkcYp8NC_
-// You can view it at https://tonscan.org/address/EQAqWnqoT4edXmwKvH_6ATnkZShrvilO5HtZmgkHkcYp8NC_
-// PigCollection contract deployed successfully at: 0:2a5a7aa84f879d5e6c0abc7ffa0139e465286bbe294ee47b599a090791c629f0
-// Sent transaction
-// Pigshop contract updated with collection address successfully
