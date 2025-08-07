4421a02d31610543e4fd438fadf0563852390e7b
aaronneocognitron <aaron.neocognitron@gmail.com>
2025-08-07 22:08:53 +0300

    fix ref link

--- src/utils/reflink.ts
+++ src/utils/reflink.ts
@@ -1,5 +1,5 @@
 const telegramAppBaseUrl = process.env.NEXT_PUBLIC_TELEGRAM_APP_URL!;
 
 export const generateRefLink = (refId: string): string => {
-  return `${telegramAppBaseUrl}start?startapp=register_${refId}`;
+  return `${telegramAppBaseUrl}?startapp=register_${refId}`;
 };
