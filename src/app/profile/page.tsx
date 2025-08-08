9109c3f667b225d452e9b4b5950a374fcce70a71
Aaron Neocognitron <aaron.neocognitron@gmail.com>
2025-08-08 13:02:18 +0300

    bump version

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -90,7 +90,7 @@ export default function ProfilePage() {
           </Button>
         </div>
         <span className="version-field">
-          v3.3.6-tst
+          v3.3.7-tst
         </span>
       </div>
     </Page>
